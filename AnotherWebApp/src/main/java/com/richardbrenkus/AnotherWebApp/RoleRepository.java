fa42970bdc3f6da9ad30bf86723182b047569eb5
richard-brenkus <richard_brenkus@yahoo.com>
2022-04-26 21:19:42 +0200

    test commit

--- AnotherWebApp/src/main/java/com/richardbrenkus/AnotherWebApp/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.richardbrenkus.AnotherWebApp;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role, String> {
-
-}
--- AnotherWebApp/src/main/java/com/richardbrenkus/AnotherWebApp/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.richardbrenkus.AnotherWebApp;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
-	
-	
-
-}
